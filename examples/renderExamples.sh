91a9018c4038803ae6089f91fe3d88f35b2c9dba
Matthew Griffith <mdg.griffith@gmail.com>
2016-01-28 16:31:25 -0500

    Script to render examples to html

---
+++ examples/renderExamples.sh
@@ -0,0 +1,6 @@
+mkdir rendered
+elm-make SideMenu.elm --output rendered/SideMenu.html
+elm-make Showcase.elm --output rendered/Showcase.html
+elm-make StackingTransforms.elm --output rendered/StackingTransforms.html
+elm-make Stagger.elm --output rendered/Stagger.html
+elm-make Chaining.elm --output rendered/Chaining.html
