249e6b4177f9b8da372101283b87a07d233fbfd8
Matthew Griffith <mdg.griffith@gmail.com>
2016-02-20 11:53:15 -0500

    Lower tolerances for springs so they finish a bit faster.
    There was a noticeable pause before a spring seemed to finish and when
    it met the technical criteria of finishing.

--- src/Html/Animation/Spring.elm
+++ src/Html/Animation/Spring.elm
@@ -25,8 +25,10 @@ type alias Physical =
   , velocity : Float
   }
 
-tolerance =
-  1.0e-3
+tolerance = 0.01
+
+vTolerance = 0.1
+  --1.0e-2
 
 update : Time -> Model -> Physical -> Physical
 update dtms spring phys =
@@ -51,7 +53,7 @@ update dtms spring phys =
       phys.position + newV * dt
 
   in
-    if (abs (spring.destination - newX)) < tolerance && abs newV < tolerance then
+    if (abs (spring.destination - newX)) < tolerance && abs newV < vTolerance then
       { phys
         | position = spring.destination
         , velocity = 0.0
@@ -66,7 +68,7 @@ update dtms spring phys =
 atRest : Model -> Physical -> Bool
 atRest spring physical =
      abs (spring.destination - physical.position) < tolerance 
-  && abs physical.velocity < tolerance
+  && abs physical.velocity < vTolerance
 
 
 duration : Model -> Physical -> Time
@@ -81,3 +83,34 @@ duration spring phys =
         )
         ( phys, 0 )
         [1..10000] -- Ticks in ms
+
+
+--x = Debug.log "no wobble duration" 
+--            <| Spring.duration { noWobble | destination = 1 }
+--                        { position = 0
+--                        , velocity = 0
+--                        }
+
+--y = Debug.log "gentle duration" 
+--            <| Spring.duration { gentle | destination = 1 }
+--                        { position = 0
+--                        , velocity = 0
+--                        }
+
+--z = Debug.log "stiff duration" 
+--            <| Spring.duration { stiff | destination = 1 }
+--                        { position = 0
+--                        , velocity = 0
+--                    }
+
+--a = Debug.log "wobbly duration" 
+--            <| Spring.duration { wobbly | destination = 1 }
+--                        { position = 0
+--                        , velocity = 0
+--                    }
+
+--b = Debug.log "fast and loose duration" 
+--            <| Spring.duration { fastAndLoose | destination = 1 }
+--                        { position = 0
+--                        , velocity = 0
+--                    }
