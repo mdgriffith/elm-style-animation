74daf6d27a28f32ecdd035de66de2d553ffc9fba
Matthew Griffith <mdg.griffith@gmail.com>
2016-05-10 21:16:59 -0400

    Chaining Example

--- examples/Chaining.elm
+++ examples/Chaining.elm
@@ -1,18 +1,11 @@
-
-
-import StartApp exposing (start)
-
+import Html.App as Html
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Effects exposing (Effects, Never)
-import Signal exposing (Address)
-import Task
-
-import Time exposing (second)
-
 import Html.Animation as UI
 import Html.Animation.Properties exposing (..)
+import Time exposing (Time, second)
+import AnimationFrame
 
 type alias Model = 
             { style : UI.Animation 
@@ -21,83 +14,80 @@ type alias Model =
 -- UPDATE
 
 type Action = ChangeColor
-            | Animate UI.Action
+            | Animate Time
 
-
-{-| Prepare a helper function manage effects and assign styles -}
-onModel =
-  UI.forwardTo 
-      Animate
-      .style
-      (\w style -> { w | style = style }) -- style setter 
-
-
-update : Action -> Model -> ( Model, Effects Action )
+update : Action -> Model -> ( Model, Cmd Action )
 update action model =
   case action of
     ChangeColor ->
-      UI.animate 
-            |> UI.props 
-                [ BackgroundColor 
-                      |> UI.toRGBA 100 100 100 1.0  
-                ] 
-        |> UI.andThen -- create a new keyframe
-            |> UI.duration (1*second)
-            |> UI.props 
-                [ BackgroundColor 
-                      |> UI.toRGBA 178 201 14 1.0 
-                ] 
-        |> UI.andThen 
-            |> UI.props 
-                [ BackgroundColor 
-                      |> UI.toRGBA 58 40 69 1.0 
-                ] 
-        |> onModel model
+      let
+        style = 
+          UI.animate 
+                |> UI.props 
+                    [ BackgroundColor 
+                          |> UI.toRGBA 100 100 100 1.0  
+                    ] 
+            |> UI.andThen -- create a new keyframe
+                |> UI.duration (1*second)
+                |> UI.props 
+                    [ BackgroundColor 
+                          |> UI.toRGBA 178 201 14 1.0 
+                    ] 
+            |> UI.andThen 
+                |> UI.props 
+                    [ BackgroundColor 
+                          |> UI.toRGBA 58 40 69 1.0 
+                    ] 
+            |> UI.on model.style
+        in
+          ( {model | style = style}
+          , Cmd.none
+          )
 
-    Animate action ->
-      onModel model action
-
-
-view : Address Action -> Model -> Html
-view address model =
-            let
-              triggerStyle = [ ("position", "relative")
-                             , ("margin", "200px auto")
-                             , ("width", "250px")
-                             , ("height", "250px")
-                             , ("text-align","center")
-                             , ("line-height", "250px")
-                             , ("color", "white")
-                             , ("cursor", "pointer")
-                             ]
-            in
-              div [ onClick address ChangeColor
-                  , style (triggerStyle ++ UI.render model.style)
-                  ]
-
-                  [ text "Click to Change Color" ]
+    Animate time ->
+     ( { model 
+            | style = UI.tick time model.style 
+        }
+    , Cmd.none)
 
 
-init : ( Model, Effects Action )
+
+view : Model -> Html Action
+view model =
+  let
+    triggerStyle = [ ("position", "relative")
+                   , ("margin", "200px auto")
+                   , ("width", "250px")
+                   , ("height", "250px")
+                   , ("text-align","center")
+                   , ("line-height", "250px")
+                   , ("color", "white")
+                   , ("cursor", "pointer")
+                   ]
+  in
+    div [ onClick ChangeColor
+        , style (triggerStyle ++ UI.render model.style)
+        ]
+
+        [ text "Click to Change Color" ]
+
+
+init : ( Model, Cmd Action )
 init = ( { style = 
               UI.init 
                   [ BackgroundColor 
                         |> UI.rgba 58 40 69 1.0 ]
          }
-       , Effects.none )
+       , Cmd.none )
 
-app =
-  StartApp.start
-    { init = init
-    , update = update
-    , view = view
-    , inputs = []
-    }
+subscriptions : Model -> Sub Action
+subscriptions model =
+  AnimationFrame.times Animate
 
 main =
-  app.html
-
-
-port tasks : Signal (Task.Task Never ())
-port tasks =
-  app.tasks
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
